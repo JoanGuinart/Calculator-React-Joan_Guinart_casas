ab0d3fbf56ccd521c4f6e5af8bbb22de97ce2ca0
Joan Guinart Casas <guinartcasasjoan@gmail.com>
2023-03-01 12:46:50 +0100

    first commit

---
+++ src/OperationButton.jsx
@@ -0,0 +1,14 @@
+import { ACTIONS } from "./App";
+
+
+export default function OperationButton({ dispatch, operation }) {
+  return (
+    <button
+      onClick={() =>
+        dispatch({ type: ACTIONS.CHOOSE_OPERATION, payload: { operation } })
+      }
+    >
+      {operation}
+    </button>
+  )
+}
